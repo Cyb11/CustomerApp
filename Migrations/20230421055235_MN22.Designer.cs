319bab34dc35486dca04a1b685840afe7ed26eae
Cyb11 <105192181+Cyb11@users.noreply.github.com>
2023-06-14 13:23:41 +0530

    Update 20230421055235_MN22.Designer.cs

--- Migrations/20230421055235_MN22.Designer.cs
+++ Migrations/20230421055235_MN22.Designer.cs
@@ -15,56 +15,16 @@ namespace CodeFirstApproach12.Migrations
     [Migration("20230421055235_MN22")]
     partial class MN22
     {
-        /// <inheritdoc />
+      
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
-#pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.5")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("CodeFirstApproach12.Models.Company", b =>
-                {
-                    b.Property<int>("CompanyId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("CompanyId"));
-
-                    b.Property<string>("ComapnyName")
-                        .IsRequired()
-                        .HasMaxLength(30)
-                        .HasColumnType("nvarchar(30)");
-
-                    b.HasKey("CompanyId");
-
-                    b.ToTable("Company", (string)null);
-                });
-
-            modelBuilder.Entity("CodeFirstApproach12.Models.Employe", b =>
-                {
-                    b.Property<int>("EmpId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("EmpId"));
-
-                    b.Property<int>("CompanyId")
-                        .HasColumnType("int");
-
-                    b.Property<string>("EmpName")
-                        .IsRequired()
-                        .HasMaxLength(30)
-                        .HasColumnType("nvarchar(30)");
-
-                    b.HasKey("EmpId");
-
-                    b.HasIndex("CompanyId");
-
-                    b.ToTable("Employe", (string)null);
-                });
+       
 
             modelBuilder.Entity("CodeFirstApproach12.Models.User", b =>
                 {
@@ -90,22 +50,7 @@ namespace CodeFirstApproach12.Migrations
                     b.ToTable("User", (string)null);
                 });
 
-            modelBuilder.Entity("CodeFirstApproach12.Models.Employe", b =>
-                {
-                    b.HasOne("CodeFirstApproach12.Models.Company", "Company")
-                        .WithMany("Employes")
-                        .HasForeignKey("CompanyId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Company");
-                });
-
-            modelBuilder.Entity("CodeFirstApproach12.Models.Company", b =>
-                {
-                    b.Navigation("Employes");
-                });
-#pragma warning restore 612, 618
+ 
         }
     }
 }
